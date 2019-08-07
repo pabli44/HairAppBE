94912095ce337e59ffeefc98ac5f409d38d8af70
pabli44 <pablivg@gmail.com>
2019-08-07 17:11:21 -0500

    First commit, project creation

---
+++ src/main/java/com/pvelilla/backend/hairapp/HairApp/HairAppApplication.java
@@ -0,0 +1,13 @@
+package com.pvelilla.backend.hairapp.HairApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HairAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HairAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pvelilla/backend/hairapp/HairApp/HairAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pvelilla.backend.hairapp.HairApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HairAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
