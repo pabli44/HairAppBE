a98fe3dde29ec2f43aad212ec12448d57fe9f14d
Pablo Velilla <pablivg@gmail.com>
2026-06-28 22:56:17 -0500

    feat: add GitHub Actions CI, upgrade to Java 21, remove H2

--- src/test/java/com/pvelilla/backend/hairapp/HairApp/HairAppApplicationTests.java
+++ src/test/java/com/pvelilla/backend/hairapp/HairApp/HairAppApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-@ActiveProfiles("test")
+@ActiveProfiles("local")
 public class HairAppApplicationTests {
 
 	@Test
